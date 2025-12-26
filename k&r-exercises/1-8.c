318ff0d9b50b8ab5870eba1acdefa8ebb1d8206f
Paolo <68049947+paolobtl@users.noreply.github.com>
2025-12-26 19:41:54 +0100

    Create 1-8.c

---
+++ k&r-exercises/1-8.c
@@ -0,0 +1,21 @@
+/*
+* Write a program to count blanks, tabs, and newlines
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    int c, tb = 0, bl = 0, nl = 0;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\n')
+            nl++;
+        if (c == '\t')
+            tb++;
+        if (c == ' ')
+            bl++;
+    }
+
+    printf("Your input has: %d new lines, %d tabs, %d blanks\n", nl, tb, bl);
+    return 0;
+}
